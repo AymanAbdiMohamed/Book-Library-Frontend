b05f13a2593c5636e20e634d07028711f3006bc0
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-12-06 22:15:23 +0300

    add BookList component to display list of books with optional page count

---
+++ components/BookList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function BookList({ books }) {
+    return (
+        <div>
+            <h2>Books</h2>
+            <ul>
+                {books.map((book) => (
+                    <li key={book.id}>
+                        {book.title} - {book.author} {b.pages ? `(${b.pages} pages)` : ''}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
