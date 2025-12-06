37b860c16614652b283885ebffb6f8b835604914
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-12-06 17:53:52 +0300

    fetch and update books in Home page

---
+++ pages/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import GenreList from '../components/GenreList.jsx';
+import BookList from '../components/BookList.jsx';
+import AddBookForm from '../components/AddBookForm.jsx';
+import { getBooks, getBooksByGenre } from '../api/api.js';
+
+export default function Home() {
+    const [books, setBooks] = useState([]);
+
+
+    useEffect(() => {
+        getBooks().then(setBooks);
+    }, []);
+
+    function onSelectGenre(id) {
+        getBooksByGenre(id).then(setBooks);
+    }
+    function onAdded(book) {
+        setBooks((prev) => [...prev, book]);
+    }
+    return (
+        <div style={{ display: 'flex', gap: '2rem',}}>
+            <div style={{ width: 200}}>
+                <GenreList onSelectGenre={onSelectGenre} />
+            </div>
+            <div style={{ flex: 1}}></div>
+                <BookList books={books} />
+                <AddBookForm onAdded={onAdded} />
+            </div>
+        </div>>
+    )
+}
