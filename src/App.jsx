44b03283a5d748b7c88da8757552f6e3bddadfa5
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-12-06 17:38:02 +0300

    App function for h1 in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { React from 'react'
+import Home from './pages/Home.jsx';
+
+function App() {
+  return (
+    <div style={{ padding: 20}}>
+      <h1>Book Library</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App
