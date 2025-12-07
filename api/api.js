fe9bd0a224af9ae5d81b40db945762b9abb4e3f2
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-12-07 11:48:40 +0300

    can ftech all books, filter by a specific genre ID, and create a new book with specific parameters

--- api/api.js
+++ api/api.js
@@ -6,3 +6,37 @@ export async function getGenres() {
     const res = await fetch(`${API}/genres/`);
     return res.json();
 }
+
+//  Genres API
+//  create a new genre (expects: { name: "GenreName" })
+export async function createGenre(data) {
+    const res = aait fetch(`${API}/genres/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+    return res.json();
+}
+
+// Books API
+// fetch all books
+export async function getBooks() {
+    const res = await fetch(`$API}/books/`);
+    return res.json();
+}
+
+// fecth books filtered by a specific genre ID
+export async function getBooksByGenre(id) {
+    const res = await fetch(`${API}/books/genre/${id}`);
+    return res.json();
+}
+
+// create a new book (expects: { title, author, genre_id, nn})
+export async function createBook(data) {
+    const res = await fetch(`${API}/books/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+    return res.json();
+}
